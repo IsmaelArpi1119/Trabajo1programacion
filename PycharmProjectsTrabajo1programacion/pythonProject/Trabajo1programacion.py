7783117a5c2b03c734e2588b0283776eb227e36f
IsmaelArpi1119 <153648826+IsmaelArpi1119@users.noreply.github.com>
2023-12-12 11:31:01 -0500

    Add files via upload

---
+++ PycharmProjectsTrabajo1programacion/pythonProject/Trabajo1programacion.py
@@ -0,0 +1,42 @@
+#Este programa sirve para para poner a prueba nuestros conocimientos aprendidos de las ultimas clases donde se usa variables y comandos enseñados por el profesor
+
+#Ejercicio 1
+#suma de dos numeros enteros
+int1=5
+int2=2
+intrespuesta=" La respuesta de la suma del int es = "
+intrespuestasuma=int1+int2 #sumamos las 2 variables y guardamos los resultados
+print(intrespuesta,intrespuestasuma) #mostramos la variablie
+#suma de 2 numeros flotante
+float1=7.5
+float2=3.7
+respuestafloat=" La respuesta de la resta del  float es = "
+floatrespuestaresta=float1-float2 #restamos las 2 variabloes y guardamos los resultados
+print(respuestafloat,floatrespuestaresta) #mostramos las variables
+#multiplica 2 numeros enteros
+multint1=9
+multint2=4
+respuestaintmult=" la respuesta de la multiplicacion del int es = "
+respuestamultint=multint1*multint2 #multiplacamos las 2 variables y guardamos el resultado
+print(respuestaintmult,respuestamultint) #mostramos las variables
+explicacionprint='La funcion print sirve para mostrar los resultados de la variable indicada'
+print(explicacionprint)
+
+#Ejercicio2
+stringnombre="Adrian Ismael Arpi Calle"
+longnombre=len(stringnombre) #realizamos la convercion de longitud del texto de la variable
+letras=" la cantidad de letras son "
+print(stringnombre,letras, longnombre) #mostramos las variables
+#el proposito de este programa es saber cuantas letras tienen los nombres ingresados
+
+#Ejercicio3
+a=1
+b=2.2
+print(a)  #mostramos la variable antes de la conversion
+print(b)  #mostramos la variable antes de la conversion
+c=float(a)  #realizamos la conversion
+d=int(b)    #realizamos la conversion
+print(c) #mostramos la variable ya convertida
+print(d) #mostramos la variable ya convertida
+#Ejercicio4
+#Listo
